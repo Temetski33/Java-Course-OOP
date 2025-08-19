c8b382197ffa004d6bf910a7e4b46c5b64572cf5
Temetski33 <teemu.laasio@metropolia.fi>
2025-08-19 14:42:18 +0300

    Add function for multiplication question

--- assignment1.3/src/Task4.java
+++ assignment1.3/src/Task4.java
@@ -2,7 +2,16 @@ public class Task4 {
 
     public static void main(String[] args) {
         generateNumber();
+        askMultiplication();
 
+    }
+
+    static void askMultiplication() {
+        int first = 2;
+        int second = 4;
+        int product = first * second;
+        System.out.println("What is " + first + " times " + second + "?");
+        System.out.println("The answer is " + product + ".");
     }
 
     static void generateNumber() {
