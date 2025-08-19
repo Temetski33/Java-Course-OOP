3448f1b9d7421c29261b68278fb4b837425f941b
Temetski33 <teemu.laasio@metropolia.fi>
2025-08-19 14:33:40 +0300

    Add function for rng

--- assignment1.3/src/Task4.java
+++ assignment1.3/src/Task4.java
@@ -1,7 +1,13 @@
 public class Task4 {
+
     public static void main(String[] args) {
-        int luku = (int) (Math.random() * 10) + 1;
-        System.out.println(luku);
+        generateNumber();
+
+    }
+
+    static void generateNumber() {
+        int number = (int) (Math.random() * 10) + 1;
+        System.out.println("Luku: " + number);
     }
 }
 
