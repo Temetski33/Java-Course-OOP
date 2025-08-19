ca87c1df327b72afadaae489d7c2c6100a523c45
Temetski33 <teemu.laasio@metropolia.fi>
2025-08-19 14:58:28 +0300

    Add rng to question function

--- assignment1.3/src/Task4.java
+++ assignment1.3/src/Task4.java
@@ -1,22 +1,34 @@
+import java.util.Scanner;
+
 public class Task4 {
 
     public static void main(String[] args) {
-        generateNumber();
+        int score = 0;
+        // generateNumber();
         askMultiplication();
 
     }
 
     static void askMultiplication() {
-        int first = 2;
-        int second = 4;
+        int first = generateNumber();
+        int second = generateNumber();
         int product = first * second;
         System.out.println("What is " + first + " times " + second + "?");
+
+        Scanner reader = new Scanner(System.in);
+        int answer = Integer.parseInt(reader.nextLine());
+        System.out.println(answer);
+
+        if (answer == product) {
+            System.out.println("Your answer is correct!");
+        }
         System.out.println("The answer is " + product + ".");
     }
 
-    static void generateNumber() {
+    static int generateNumber() {
         int number = (int) (Math.random() * 10) + 1;
         System.out.println("Luku: " + number);
+        return number;
     }
 }
 
