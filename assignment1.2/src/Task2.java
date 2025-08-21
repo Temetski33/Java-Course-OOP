7fce51993e706071300f8d3d4760ab5d3915e5b5
Temetski33 <teemu.laasio@metropolia.fi>
2025-08-21 10:55:23 +0300

    Small cleanup

--- assignment1.2/src/Task2.java
+++ assignment1.2/src/Task2.java
@@ -5,13 +5,14 @@ public class Task2 {
         System.out.println("Let's count the length of a hypotenuse.");
 
         System.out.println("Give the length of the A leg: ");
-
         Scanner reader = new Scanner(System.in);
         double a = Double.parseDouble(reader.nextLine());
 
         System.out.println("Give the length of the B leg: ");
         double b = Double.parseDouble(reader.nextLine());
+
         double hypotenuse = Math.sqrt(a*a + b*b);
+
         System.out.printf("The length of the hypotenuse is " + hypotenuse + "lol");
     }
 }
