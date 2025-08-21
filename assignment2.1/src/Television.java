ef91fabadd80686c9e43f8974828b21b90acf874
Temetski33 <teemu.laasio@metropolia.fi>
2025-08-21 13:05:36 +0300

    Task 1 create initial structure

---
+++ assignment2.1/src/Television.java
@@ -0,0 +1,20 @@
+public class Television {
+    int setChannel(int channel) {
+        System.out.println("haha");
+        return channel;
+    }
+
+    int getChannel() {
+        System.out.println("haha");
+        return 0;
+    }
+
+    void pressOnOff() {
+
+    }
+
+    boolean isOn() {
+
+        return false;
+    }
+}
