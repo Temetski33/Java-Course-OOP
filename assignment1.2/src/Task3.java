62134bf0b226f6282fb057b137c7c753ca2e15ba
Temetski33 <teemu.laasio@metropolia.fi>
2025-08-25 00:18:42 +0300

    Fixed print to correct decimal places

--- assignment1.2/src/Task3.java
+++ assignment1.2/src/Task3.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
-        System.out.println("Give the mass in g: ");
+        System.out.println("Weight (g): ");
         Scanner reader = new Scanner(System.in);
         double grams = Double.parseDouble(reader.nextLine());
 
@@ -10,6 +10,6 @@ public class Task3 {
         double naula = luoti / 32;
         double leiviska = naula / 20;
 
-        System.out.printf(grams + " grams is " + leiviska + " leiviska, " + naula + " naula, and " + luoti + " luoti.");
+        System.out.printf("%.0f grams is %d leiviska, %d naula, and %.2f luoti. %n", grams, (int) leiviska, (int) naula, luoti);
     }
 }
