f982202fcbd0e34dab55fd7cdd1c060b25dea616
Temetski33 <teemu.laasio@metropolia.fi>
2025-08-25 09:39:41 +0300

    Add rng function

--- assignment1.4/src/Task1.java
+++ assignment1.4/src/Task1.java
@@ -1,5 +1,21 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("Goood morning !?");
+
+        int yks = generateNumber();
+        int kaks = generateNumber();
+
+        System.out.println(yks);
+        System.out.println(kaks);
+
     }
+
+    static int generateNumber() {
+        return (int) (Math.random() * 10) + 1;
+    }
+
+
+
 }
