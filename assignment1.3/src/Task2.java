f2bebed3730bdb6e56b3b4adca03fd4bb747b8d8
Temetski33 <teemu.laasio@metropolia.fi>
2025-08-26 05:30:23 +0300

    Add conversions and for loop

--- assignment1.3/src/Task2.java
+++ assignment1.3/src/Task2.java
@@ -9,8 +9,14 @@ public class Task2 {
         int decimal = 0;
 
         for (int i = 1; i <= iterations; i++) {
-            char result = reverse.charAt(i-1);
-            System.out.print(result);
+            int currentDigit = i-1;
+            char x = reverse.charAt(currentDigit);
+            int y = x - '0';
+
+            int power = (int) Math.pow(2, currentDigit);
+            decimal = decimal + power;
         }
+
+        System.out.println("Converted to decimal: " + decimal);
     }
 }
