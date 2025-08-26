e757e34f375d6576b8214c1c82d668f321d441da
Temetski33 <teemu.laasio@metropolia.fi>
2025-08-26 06:06:14 +0300

    Add error message if faulty range

--- assignment1.3/src/Task3.java
+++ assignment1.3/src/Task3.java
@@ -2,8 +2,13 @@ public class Task3 {
     public static void main(String[] args) {
         System.out.println("hello to all the world");
 
-        int start = 27;
-        int end = 50;
+        int start = 270;
+        int end = 500;
+
+        if (start > end) {
+            System.out.println("Start cannot be larger than end.");
+            return;
+        }
 
         for (int i = start; i <= end; i++) {
             if (checkPrime(i)) {
@@ -18,7 +23,6 @@ public class Task3 {
         if (n <= 1)
             return false;
 
-        // Check from 2 to sqrt(n)
         for (int i = 2; i <= Math.sqrt(n); i++)
             if (n % i == 0)
                 return false;
