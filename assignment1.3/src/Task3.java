447bb5dded204514152d9e35e88412c6392dba3d
Temetski33 <teemu.laasio@metropolia.fi>
2025-08-26 05:58:57 +0300

    Add prime function and for loop

--- assignment1.3/src/Task3.java
+++ assignment1.3/src/Task3.java
@@ -1,5 +1,28 @@
 public class Task3 {
     public static void main(String[] args) {
         System.out.println("hello to all the world");
+
+        int start = 27;
+        int end = 50;
+
+        for (int i = start; i <= end; i++) {
+            if (checkPrime(i)) {
+                System.out.println(i);
+            }
+        }
+
+
+    }
+
+    static boolean checkPrime(int n) {
+        if (n <= 1)
+            return false;
+
+        // Check from 2 to sqrt(n)
+        for (int i = 2; i <= Math.sqrt(n); i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
     }
 }
