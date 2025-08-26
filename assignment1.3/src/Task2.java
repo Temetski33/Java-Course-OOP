c5c11073c59b8494aac4ac26c77f41f491f635fd
Temetski33 <teemu.laasio@metropolia.fi>
2025-08-26 05:32:15 +0300

    Add binary to decimal conversion

--- assignment1.3/src/Task2.java
+++ assignment1.3/src/Task2.java
@@ -3,7 +3,6 @@ public class Task2 {
         String input = "10110";
         System.out.println(input);
         String reverse = new StringBuilder(input).reverse().toString();
-        System.out.println(reverse);
 
         int iterations = input.length();
         int decimal = 0;
@@ -13,8 +12,10 @@ public class Task2 {
             char x = reverse.charAt(currentDigit);
             int y = x - '0';
 
-            int power = (int) Math.pow(2, currentDigit);
-            decimal = decimal + power;
+            if (y == 1) {
+                int power = (int) Math.pow(2, currentDigit);
+                decimal = decimal + power;
+            }
         }
 
         System.out.println("Converted to decimal: " + decimal);
