b4549949e68a32c9a5a627f9a0fe6279577f4b58
Temetski33 <teemu.laasio@metropolia.fi>
2025-08-26 06:58:30 +0300

    Initial commit (copied from task2)

---
+++ assignment1.4/src/Task3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println("Enter the size of the array: ");
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+
+        int[] numbers = new int[size];
+
+        System.out.println("Enter the integers into the array: ");
+
+        for (int i = 0; i <= size - 1; i++){
+            System.out.println("Enter integer " + (i+1) + ": ");
+            numbers[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < size; i++) {
+            System.out.println(numbers[i]);
+        }
+
+
+    }
+}
