25777e0eb68070bcf8bad41414b4ba32a3a44f3e
Temetski33 <teemu.laasio@metropolia.fi>
2025-08-26 06:57:04 +0300

    Add array creation

--- assignment1.4/src/Task2.java
+++ assignment1.4/src/Task2.java
@@ -2,13 +2,21 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-        System.out.println("How many integers?");
+        System.out.println("Enter the size of the array: ");
         Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
 
+        int[] numbers = new int[size];
 
-        for (int i = 1; i <= size; i++){
-            System.out.println("Enter integer " + i + ": ");
+        System.out.println("Enter the integers into the array: ");
+
+        for (int i = 0; i <= size - 1; i++){
+            System.out.println("Enter integer " + (i+1) + ": ");
+            numbers[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < size; i++) {
+            System.out.println(numbers[i]);
         }
 
 
