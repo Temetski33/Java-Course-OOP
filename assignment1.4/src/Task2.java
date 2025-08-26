4a7169a48a0f5202d37dbe2129897c6f41a158df
Temetski33 <teemu.laasio@metropolia.fi>
2025-08-26 06:39:58 +0300

    Add user input

--- assignment1.4/src/Task2.java
+++ assignment1.4/src/Task2.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("How many integers?");
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+
+
+        for (int i = 1; i <= size; i++){
+            System.out.println("Enter integer " + i + ": ");
+        }
+
+
     }
 }
