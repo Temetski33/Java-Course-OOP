7972e16fabd3480a58e7399da357e833fb7b0919
Temetski33 <teemu.laasio@metropolia.fi>
2025-08-26 04:55:40 +0300

    Update prints

--- assignment1.3/src/Task4.java
+++ assignment1.3/src/Task4.java
@@ -22,11 +22,11 @@ public class Task4 {
         int answer = Integer.parseInt(reader.nextLine());
 
         if (answer == product) {
-            System.out.println("Your answer is correct!");
+            System.out.println("Correct.");
             return 1;
         }
         else {
-            System.out.println("Not quite!");
+            System.out.println("Incorrect.");
             System.out.println("The correct answer is " + product + ".");
             return 0;
         }
@@ -34,19 +34,22 @@ public class Task4 {
     }
 
     static int startGame() {
-        System.out.println("Welcome to game!!!!!!");
+        System.out.println("Welcome to multiplication table exam!");
+        System.out.println("You must answer ten questions correctly to pass.");
+        System.out.println();
 
         int surpa = 0;
         for (int i = 1; i <= 10; i++) {
             surpa = surpa + askMultiplication();
-            System.out.println(surpa + " out of " + i + " answers correct so far.");
+            System.out.println(surpa + "/" + i + " correct.");
+            System.out.println();
         }
         if (surpa == 10) {
-            System.out.println("hurrau");
+            System.out.println("Congratulations on mastering the multiplication table!");
             return 1;
         }
         else {
-            System.out.println("nt yri");
+            System.out.println("Try again.");
             return 0;
 
         }
