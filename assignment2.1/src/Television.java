07be588cc19a0733b02ed3df5239c849d2a476e1
Temetski33 <teemu.laasio@metropolia.fi>
2025-08-28 02:31:49 +0300

    Fixed setChannel to not return

--- assignment2.1/src/Television.java
+++ assignment2.1/src/Television.java
@@ -2,12 +2,11 @@ public class Television {
 
     int currentChannel = 0;
 
-    int setChannel(int channel) {
+    void setChannel(int channel) {
         currentChannel = channel;
         if (currentChannel > 10) {
             currentChannel = 1;
         }
-        return currentChannel;
     }
 
     int getChannel() {
