f049296f50f4b83392c552a8c89ac07d923f6f7c
Temetski33 <teemu.laasio@metropolia.fi>
2025-08-28 09:06:18 +0300

    Add inputs and prints

--- assignment2.1/src/CoffeeMakerDriver.java
+++ assignment2.1/src/CoffeeMakerDriver.java
@@ -3,11 +3,24 @@ public class CoffeeMakerDriver {
         CoffeeMaker mokkaMaster = new CoffeeMaker();
 
         mokkaMaster.pressOnOff();
+        if (mokkaMaster.isOn()){
+            System.out.println("Coffee maker is on");
+        } else {
+            System.out.println("Coffee maker is off");
+        }
 
         mokkaMaster.setType("espresso");
+        System.out.println("Coffee type is " + mokkaMaster.getCoffeeType());
 
         mokkaMaster.setAmount(50);
+        System.out.println("Coffee amount is " + mokkaMaster.getCoffeeAmount() + " ml");
 
         mokkaMaster.pressOnOff();
+        if (mokkaMaster.isOn()){
+            System.out.println("Coffee maker is on");
+        } else {
+            System.out.println("Coffee maker is off");
+        }
     }
+
 }
