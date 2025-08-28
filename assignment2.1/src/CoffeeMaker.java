4dbf8903b0617d71f8a5ed0dda33cbafbd513a5a
Temetski33 <teemu.laasio@metropolia.fi>
2025-08-28 09:06:41 +0300

    Add structure

--- assignment2.1/src/CoffeeMaker.java
+++ assignment2.1/src/CoffeeMaker.java
@@ -1,2 +1,32 @@
 public class CoffeeMaker {
+    private boolean power = false;
+    private int coffeeAmount = 0;
+    private String coffeeType = "";
+
+    void pressOnOff() {
+        power = !power;
+    }
+
+    boolean isOn() {
+        return power;
+    }
+
+    void setAmount(int amount) {
+        if (10 <= amount && amount <= 80) {
+            coffeeAmount = amount;
+        }
+
+    }
+
+    void setType(String type) {
+       coffeeType = type;
+    }
+
+    int getCoffeeAmount() {
+        return coffeeAmount;
+    }
+
+    String getCoffeeType() {
+        return coffeeType;
+    }
 }
