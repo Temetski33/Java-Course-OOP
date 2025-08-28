a345658e391b5af185867e582203aa1b299cea49
Temetski33 <teemu.laasio@metropolia.fi>
2025-08-28 08:58:10 +0300

    Add structure

--- assignment2.1/src/CoffeeMakerDriver.java
+++ assignment2.1/src/CoffeeMakerDriver.java
@@ -1,2 +1,13 @@
 public class CoffeeMakerDriver {
+    public static void main(String[] args) {
+        CoffeeMaker mokkaMaster = new CoffeeMaker();
+
+        mokkaMaster.pressOnOff();
+
+        mokkaMaster.setType("espresso");
+
+        mokkaMaster.setAmount(50);
+
+        mokkaMaster.pressOnOff();
+    }
 }
