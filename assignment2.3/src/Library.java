b2390fbb019a4974909c28e2341590d17aca7c7a
Temetski33 <teemu.laasio@metropolia.fi>
2025-09-01 01:49:15 +0300

    Fix more prints

--- assignment2.3/src/Library.java
+++ assignment2.3/src/Library.java
@@ -12,7 +12,7 @@ public class Library {
         int counter = 1;
         System.out.println("Library catalog:");
         for (Book book: this.books) {
-            System.out.println(counter + ". Title: \"" + book.getTitle() + "\", Author: \"" + book.getAuthor() + "\" Year: " + book.getYear());
+            System.out.println(counter + ". Title: \"" + book.getTitle() + "\", Author: \"" + book.getAuthor() + "\", Year: " + book.getYear());
             counter++;
         }
         System.out.println();
@@ -22,7 +22,8 @@ public class Library {
         System.out.println("Books by author \"" + author + "\":");
         for (Book book: this.books) {
             if (author.equals(book.getAuthor())) {
-                System.out.println("Title: \"" + book.getTitle() + "\", Author: \"" + book.getAuthor() + "\" Year: " + book.getYear());
+                System.out.println("Title: \"" + book.getTitle() +"\", Year: " + book.getYear());
+
 
             }
         }
