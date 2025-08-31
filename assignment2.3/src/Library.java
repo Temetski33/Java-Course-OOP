b90b4e2d9b337393240acf360d46e53e957c3911
Temetski33 <teemu.laasio@metropolia.fi>
2025-09-01 01:26:21 +0300

    Add findbooks method

--- assignment2.3/src/Library.java
+++ assignment2.3/src/Library.java
@@ -12,6 +12,17 @@ public class Library {
         int counter = 1;
         for (Book book: this.books) {
             System.out.println(counter + "Title: \"" + book.getTitle() + "\", Author: \"" + book.getAuthor() + "\" Year: " + book.getYear());
+            counter++;
+        }
+    }
+
+    void findBooksByAuthor(String author) {
+        System.out.println("Books by author \"" + author + "\":");
+        for (Book book: this.books) {
+            if (author.equals(book.getAuthor())) {
+                System.out.println("Title: \"" + book.getTitle() + "\", Author: \"" + book.getAuthor() + "\" Year: " + book.getYear());
+
+            }
         }
     }
 }
