dfa604486b9f0f3d78e041b67582a2184fee8f96
Temetski33 <teemu.laasio@metropolia.fi>
2025-09-01 01:36:18 +0300

    Add main class

---
+++ assignment2.3/src/LibraryMain.java
@@ -0,0 +1,17 @@
+public class LibraryMain {
+    public static void main(String[] args) {
+        Library library = new Library();
+
+        Book dune = new Book("Dune", "Frank Herbert", "1965");
+        Book duneMessiah = new Book("Dune Messiah", "Frank Herbert", "1969");
+        Book wayOfKings = new Book("The Way of Kings", "Brandon Sanderson", "2014");
+
+        library.addBook(dune);
+        library.addBook(duneMessiah);
+        library.addBook(wayOfKings);
+
+        library.displayBooks();
+
+        library.findBooksByAuthor("Brandon Sanderson");
+    }
+}
