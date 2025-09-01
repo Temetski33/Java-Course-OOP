338aa424626cf92a27bacd8fc1f9bfd1eb02a5f5
Temetski33 <teemu.laasio@metropolia.fi>
2025-09-02 01:58:32 +0300

    Fix bus inputs

--- assignment3.2/src/VehicleDemo.java
+++ assignment3.2/src/VehicleDemo.java
@@ -2,7 +2,7 @@ public class VehicleDemo {
     public static void main(String[] args) {
         Vehicle car = new Car("Petrol", "Red");
         Vehicle motorcycle = new Motorcycle("Gasoline", "Black");
-        Vehicle bus = new Bus("Diesel", "Yellow");
+        Vehicle bus = new Bus("Diesel", 40);
 
         System.out.println("Vehicle demonstration");
         System.out.println();
