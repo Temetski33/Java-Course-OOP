f1cf782a3ddcc2a6006b96743b21b4cb584a45d7
Temetski33 <teemu.laasio@metropolia.fi>
2025-09-01 05:06:03 +0300

    Add structure

--- assignment2.3/src/User.java
+++ assignment2.3/src/User.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private String name;
+    private String age;
+    private List<String> borrowedBooks;
+
+    public User(String name, String age) {
+        this.name = name;
+        this.age = age;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public void addBorrowedBook(String book) {
+        borrowedBooks.add(book);
+    }
+
+    public void removeBorrowedBook(String book) {
+        borrowedBooks.remove(book);
+    }
 }
