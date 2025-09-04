e964355275455d80f2c253bd8474b305a5141ff2
Temetski33 <teemu.laasio@metropolia.fi>
2025-09-04 08:17:17 +0300

    Add prints to methods

--- library/src/system/Library.java
+++ library/src/system/Library.java
@@ -12,22 +12,29 @@ public class Library {
     public void addBook(Book book) {
         // Add book to the library
         books.add(book);
+        System.out.println(book.getTitle() + " by " + book.getAuthor() + " (ISBN: " + book.getIsbn() + ") added to library.");
+        System.out.println();
     }
 
     public void addMember(LibraryMember member) {
         // Add member to the library
         members.add(member);
+        System.out.println(member.getName() + " (" + member.getMemberId() + ") added to members list.");
+        System.out.println();
     }
 
     public void borrowBook(LibraryMember member, Book book) {
         // Borrow book from the library
         books.remove(book);
         member.borrowBook(book);
+        System.out.println("User " + member.getName() + " borrowed " + book.getTitle() + " by " + book.getAuthor() + ".");
     }
 
     public void returnBook(LibraryMember member, Book book) {
         // Return book to the library
         member.returnBook(book);
         books.add(book);
+        System.out.println(member.getName() + " returned " + book.getTitle() + ".");
+        System.out.println();
     }
 }
