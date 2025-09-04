b3aec8b20ece99db9c3b262628179ba8a2003992
Temetski33 <teemu.laasio@metropolia.fi>
2025-09-04 07:18:39 +0300

    Fix constructor method

--- library/src/model/Book.java
+++ library/src/model/Book.java
@@ -7,7 +7,7 @@ public class Book {
 
     // Constructor, getters, setters
 
-    public void Book(String title, String author, String isbn) {
+    public Book(String title, String author, String isbn) {
         this.title = title;
         this.author = author;
         this.isbn = isbn;
