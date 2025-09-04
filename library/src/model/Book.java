0ec3e0a5be25a02ef09dc55e7dde3104c2a9ba0a
Temetski33 <teemu.laasio@metropolia.fi>
2025-09-04 07:17:50 +0300

    Add constructor, setters and getters

--- library/src/model/Book.java
+++ library/src/model/Book.java
@@ -6,4 +6,34 @@ public class Book {
     private String isbn;
 
     // Constructor, getters, setters
+
+    public void Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 }
