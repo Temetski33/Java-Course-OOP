f4f54579c60e9d442a5d42a3bb4f97033f828ffd
Temetski33 <teemu.laasio@metropolia.fi>
2025-09-04 07:52:57 +0300

    Add first instance of book and library

--- library/src/Main.java
+++ library/src/Main.java
@@ -1,7 +1,17 @@
+import system.Library;
+import model.Book;
+import model.LibraryMember;
+
 public class Main {
     public static void main(String[] args) {
         // Create instances of Library, Book, LibraryMember
         // Add books and members to the library
         // Perform borrowing and returning operations
+
+        Library library = new Library();
+
+        Book dune = new Book("Dune", "Frank Herbert", "DFN231");
+
+
     }
 }
