071c221010a182710c57b3926213b89ac54c33a1
Temetski33 <teemu.laasio@metropolia.fi>
2025-09-04 07:29:51 +0300

    Add constructor and books list

--- library/src/model/LibraryMember.java
+++ library/src/model/LibraryMember.java
@@ -1,9 +1,16 @@
 package model;
 
+import java.util.LinkedList;
+
 public class LibraryMember {
     private String name;
     private int memberId;
-    // List<Book> borrowedBooks
+    LinkedList<Book> borrowedBooks = new LinkedList<>();
 
     // Constructor, getters, setters
+
+    public LibraryMember(String name, int memberId) {
+        this.name = name;
+        this.memberId = memberId;
+    }
 }
