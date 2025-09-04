678467a09035e3453e35d95886cbe062857691ed
Temetski33 <teemu.laasio@metropolia.fi>
2025-09-04 07:41:30 +0300

    Add getters and setters

--- library/src/model/LibraryMember.java
+++ library/src/model/LibraryMember.java
@@ -13,4 +13,20 @@ public class LibraryMember {
         this.name = name;
         this.memberId = memberId;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
 }
