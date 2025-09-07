e6fded24f8f85760d38bf4acb09f39e8950b6512
Temetski33 <teemu.laasio@metropolia.fi>
2025-09-07 23:00:19 +0300

    Add initial structure

--- assignment2.2/src/GroceryListManager.java
+++ assignment2.2/src/GroceryListManager.java
@@ -4,4 +4,19 @@ public class GroceryListManager {
     private ArrayList<String> groceryList = new ArrayList<>();
 
     // Add methods here
+    public void addItem(String item) {
+        groceryList.add(item);
+    }
+
+    public void removeItem(String item) {
+        groceryList.remove(item);
+    }
+
+    public void displayList() {
+        System.out.println(groceryList);
+    }
+
+    public boolean checkItem(String item){
+        return groceryList.contains(item);
+    }
 }
